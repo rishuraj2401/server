2ea18c5c8d63a155f4ff3a39fe96f74b71354fb7
rishuraj2401 <rishuraj2401sinha@gmail.com>
2022-08-07 22:01:18 +0530

    ignore node_modules

--- router/auth.js
+++ router/auth.js
@@ -1,7 +1,6 @@
 const express=require("express");
 const router =express.Router();
-router.get("/", (req ,res )=>
-res.send("hellio register hiiiii"));
+
 // router.post("/register" ,(req,res)=>
 // {
 // console.log(req.body)
