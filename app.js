e1eb4cab192a3d04361cd6e85dc50a79fd4eb730
rishuraj2401 <99868466+rishuraj2401@users.noreply.github.com>
2022-08-21 17:02:37 +0530

    up

--- app.js
+++ app.js
@@ -258,15 +258,17 @@ app.put("/Ret", async (req, res) => {
 
 }
 );
+   const path = require("path");
 if (process.env.NODE_ENV == "production") {
-    app.use(express.static("my-project/build"));
-    const path = require("path");
+   
+ 
+    app.use(express.static(path.resolve(__dirname, "./my-project/build")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'my-project', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, "./my-project/build", "index.html"))
     })
 }
 app.use(express.urlencoded({ extended: false }));
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 var server = app.listen(port, function () {
     console.log('Node server is totally running...');
 });
